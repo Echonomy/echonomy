df876bc9ca7fce38e9dd8bfdb36c180ea6f71db1
Piotr Adamczyk <xeno@xeno.yt>
2024-03-16 12:15:40 +0000

    Expose tRPC in the console for debugging purposes

--- src/utils/trpc.tsx
+++ src/utils/trpc.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import {
+  createTRPCClient,
+  createTRPCClientProxy,
+  loggerLink,
+  unstable_httpBatchStreamLink,
+} from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 import SuperJSON from "superjson";
@@ -43,7 +48,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
           },
         }),
       ],
-    })
+    }),
   );
 
   return (
@@ -59,4 +64,26 @@ function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+if (window) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+  (window as any).trpc = createTRPCClient<AppRouter>({
+    links: [
+      loggerLink({
+        enabled: (op) =>
+          process.env.NODE_ENV === "development" ||
+          (op.direction === "down" && op.result instanceof Error),
+      }),
+      unstable_httpBatchStreamLink({
+        transformer: SuperJSON,
+        url: getBaseUrl() + "/api/trpc",
+        headers: () => {
+          const headers = new Headers();
+          headers.set("x-trpc-source", "nextjs-react");
+          return headers;
+        },
+      }),
+    ],
+  });
 }
