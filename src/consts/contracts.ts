fe9723249fcf6985506ea2545352dc13f2998c32
Piotr Adamczyk <xeno@xeno.yt>
2024-03-17 04:52:58 +0000

    Add approve, remove value attachement

--- src/consts/contracts.ts
+++ src/consts/contracts.ts
@@ -4,9 +4,10 @@ import { type SupportedNetworkId } from "~/utils/networks";
 
 export const contractAddress: Record<
   SupportedNetworkId,
-  Record<Exclude<ContractName, "Safe">, `0x${string}`>
+  Record<Exclude<ContractName, "Safe" | "IERC20">, `0x${string}`>
 > = {
   [baseSepolia.id]: {
     EchonomySongRegistry: "0xe63ae6BfC9Ad7F225750F8ADD26e87BC69A9f574",
+    USDC: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
   },
 };
