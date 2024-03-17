2931ac4018b686e12d50f587db9ef9c7b9ace273
Piotr Adamczyk <xeno@xeno.yt>
2024-03-17 04:05:01 +0000

    Remove signed typed dat code

--- src/utils/signature.ts
+++ src/utils/signature.ts
@@ -1,4 +1,3 @@
-import deterministicJsonStringify from "json-stringify-deterministic";
 import { type AppRouter } from "~/server/api/root";
 
 type Paths<T> = T extends object
@@ -17,29 +16,3 @@ export const signatureProtectedMethods: string[] = [
   Paths<AppRouter["_def"]["procedures"]>,
   `${string}._def${string}`
 >[];
-
-export function generateSignedProcedurePayload({
-  input,
-  path,
-  timestamp,
-}: {
-  input: unknown;
-  path: string;
-  timestamp: Date;
-}) {
-  const deterministicInput = deterministicJsonStringify(input);
-  return `Sign to authorize Echonomy call to ${path} with input: ${deterministicInput} at ${timestamp.toISOString()}`;
-}
-
-export const getTypedDataDomainForChainId = (chainId: number) =>
-  ({
-    name: "Echonomy",
-    version: "1",
-    chainId,
-    verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
-  }) as const;
-
-// The named list of all type definitions
-export const typedDataTypes = {
-  Generic: [{ name: "contents", type: "string" }],
-} as const;
