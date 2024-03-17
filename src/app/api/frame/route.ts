8f7ebce43be2a84b90a43b93b5633d703b859228
Muneer Fergiani <m.fergiani.m@gmail.com>
2024-03-17 05:16:13 +0000

    hooked up our data to frame

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-
+import { api } from "~/utils/trpc";
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const searchParams = req.nextUrl.searchParams
   const id = String(searchParams.get("id"));
@@ -7,26 +7,19 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   const nextId = idAsNumber + 1
 
-  if(idAsNumber === 7){
-      return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame 7</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/7.png" />
-    <meta property="fc:frame:button:1" content="Visit CosmicCowboys.cloud" />
-    <meta property="fc:frame:button:1:action" content="post_redirect" />
-    <meta property="fc:frame:button:2" content="Learn How this was made" />
-    <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
-  </head></html>`);
-  } else {
+  const songData = api.songs.get.useQuery({
+    id: Number(id),
+  });
+
+
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/${id}.png" />
+    <meta property="fc:frame:image" content="${songData.data?.artwork}" />
     <meta property="fc:frame:button:1" content="Next Page" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+    <meta property="fc:frame:post_url" content="https://echonomy.vercel.app/api/frame?id=${nextId}" />
   </head></html>`);
-  }
+  
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
