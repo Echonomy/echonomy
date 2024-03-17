d0bb197d749e2835a1d8742780a4518ba58cee2c
Muneer Fergiani <m.fergiani.m@gmail.com>
2024-03-17 04:51:43 +0000

    updated the post url

--- src/app/frame/page.tsx
+++ src/app/frame/page.tsx
@@ -9,7 +9,7 @@ const frameMetadata = getFrameMetadata({
     }
   ],
   image: `https://cdn.discordapp.com/attachments/1043649157530910791/1217914678865559693/IMAGE_2024-03-14_191803.jpg?ex=6605c26f&is=65f34d6f&hm=57abb49a21bef1fcd2777caf99da18d9c7742255d4cce8a54f096d993b1a9f32&`,
-  post_url: `localhost:3000/api/frame?id=1`,
+  post_url: `https://echonomy.vercel.app/api/frame?id=1`,
 });
 
 
